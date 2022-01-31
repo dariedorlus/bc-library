1117917af083932ebb5656507176da47969bb8cd
Tda <tdorluss@gmail.com>
2022-01-31 16:54:58 -0500

    added post, get + connected to firestore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+const credentials = require("../credentials.json");
+
+initializeApp({
+    credential: cert(credentials),
+})
+
+const db = getFirestore();
+
 
 const app = express();
 app.use(express.json());
 
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
+    const userCollection = db.collection("users");
+
+    userCollection
+    .get()
+    .then(snapshot => {
+
+        // const users = []
+        // snapshot.forEach(doc => {
+        //     users.push({id: doc.id, ...doc.data()})
+        // })
+
+        response.send(snapshot.docs)
+    })
+
+    // response.send('Hello World!');
 })
 
 app.post('/users', (req, res) => {
@@ -26,6 +51,7 @@ app.post('/users', (req, res) => {
 
     res.send(result);
 })
+
 
 
 app.listen(3000, () => {
