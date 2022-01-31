5040f0ebd781199b20d650907caaf4c4ed537f89
Tda <tdorluss@gmail.com>
2022-01-31 15:19:40 -0500

    getting and posting api

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+})
+
+app.post('/users', (req, res) => {
+    const {name, age, email} = req.body;
+    
+    // const {name} = req.body;
+    // const name = req.body.name;
+
+    const user = { name, age, email };
+
+    // const user = {
+    //     fullName: name, 
+    //     age: age,
+    //     email: email
+    // }
+
+    const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}`;
+
+    res.send(result);
+})
+
+
+app.listen(3000, () => {
+    console.log('We be listening on 3000');
+})
